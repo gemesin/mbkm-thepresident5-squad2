5c21bcb8b928492813afbdfe0a58de1e60076b44
TegarCode <105096476+TegarCode@users.noreply.github.com>
2023-11-08 14:14:20 +0700

    Update db_config.js
    kesalahan ges disini server

--- confing/db_config.js
+++ confing/db_config.js
@@ -1,9 +1,9 @@
 const dbConfig = {
-    host: '195.35.32.179',
+    host: 'msib-mysql-db',
     user: 'root',
     password: 'root@msib',
     db: 'msib_squad2',
-    Port: 4000,
+    Port: 3306,
     dialect: 'mysql'
 }
 
