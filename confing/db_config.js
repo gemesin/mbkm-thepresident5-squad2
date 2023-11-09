ae86259781764be57f91450cfa8d01efa327253d
TegarCode <105096476+TegarCode@users.noreply.github.com>
2023-11-09 21:53:42 +0700

    Add files via upload
    kehapus ges maaf

---
+++ confing/db_config.js
@@ -0,0 +1,10 @@
+const dbConfig = {
+    host: '195.35.32.179',
+    user: 'root',
+    password: 'root@msib',
+    db: 'msib_squad2',
+    port: 4000,
+    dialect: 'mysql'
+}
+
+module.exports = dbConfig;
