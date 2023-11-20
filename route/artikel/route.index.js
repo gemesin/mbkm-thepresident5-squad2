e7bef161bbaa95e9ec678ecc205179422d7d23bc
Ilhamjul <hamzul1302@gmail.com>
2023-11-21 02:42:10 +0700

    Menyimpan perubahan lokal sebelum menarik dari branch raihana

--- route/artikel/route.index.js
+++ route/artikel/route.index.js
@@ -1,7 +1,34 @@
 const express = require('express');
-const { Sequelize, DataTypes } = require('sequelize');
-const app = express();
+
 const router = express.Router();
 
+const { SECRET_KEY } = require("../../items");
+const { articleModel } = require('../../models');
+
+const { body, validationResult } = require("express-validator");
+const erorHandlerMiddleware = require('../../middleware/error-handling');
+
+// Endpoint untuk tambah artikel post
+router.post('/add_article', async (req, res) => {
+  try {
+    const data = req.body;
+
+    const newArtikel = await Artikel.create({
+      title: data.title,
+      name: data.name,
+      image: data.image,
+      desc: data.desc,
+      article: data.article,
+    });
+
+    res.status(201).json({ message: 'Article added successfully' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'It was a mistake to add the article' });
+  }
+});
 
 
+router.use(erorHandlerMiddleware);
+
+module.exports = router;
