590227f871a97e9c4cf3c23ba643ddbd68e1109b
Tegar Oktavianto Simbolon <oktaviantotegar2@gmail.com>
2023-11-20 11:14:51 +0700

    memperbaiki config

--- confing/db_config.js
+++ confing/db_config.js
@@ -1,10 +1,10 @@
 const dbConfig = {
-    host: 'msib-mysql-db',
-     user: 'root',
-     password: 'root@msib',
-     db: 'msib_squad2',
-     port: 3306,
-     dialect: 'mysql'
- }
- 
- module.exports = dbConfig;
+    host: '195.35.32.179',
+    user: 'root',
+    password: 'root@msib',
+    db: 'msib_squad2',
+    port: 4000,
+    dialect: 'mysql'
+}
+
+module.exports = dbConfig;
