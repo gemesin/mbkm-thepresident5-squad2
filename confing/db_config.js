12094d66a85695e5d0eda94fcffeeef4fb16dc1e
Tegar Oktavianto Simbolon <oktaviantotegar2@gmail.com>
2023-11-27 00:25:11 +0700

    terubah

--- confing/db_config.js
+++ confing/db_config.js
@@ -1,9 +1,9 @@
 const dbConfig = {
-   host: '195.35.32.179',
+   host: 'msib-mysql-db',
     user: 'root',
     password: 'root@msib',
     db: 'msib_squad2',
-    port: 4000,
+    port: 3306,
     dialect: 'mysql'
 }
 
