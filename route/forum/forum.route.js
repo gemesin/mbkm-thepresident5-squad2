3f23f331b3134c4834d5311ffe529ee0cf15e610
Tegar Oktavianto Simbolon <oktaviantotegar2@gmail.com>
2023-11-30 23:58:02 +0700

    menyelesaikan fitur untuk tambah ulasan

--- route/forum/forum.route.js
+++ route/forum/forum.route.js
@@ -1,17 +1,119 @@
 const express = require('express');
 const router = express.Router();
 const { protect } = require('../../middleware/middleware');
-const { SECRET_KEY } = require("../../items");
-const { ForumModel, commentModel, userModel } = require('../../models');
-const { body, validationResult } = require("express-validator");
+const { ForumModel } = require('../../models');
 const erorHandlerMiddleware = require('../../middleware/error-handling');
-const ulasanmodulModel = require('../../models/ulasanmodul.model');
+const multer = require('multer');
+const path = require('node:path');
+const { uptime } = require('node:process');
+const fs = require('fs').promises;
 
-router.get('/test',  async (req, res) => {
-    res.status(201).json({ message: 'lms access!' });
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'images/'); // Direktori tempat menyimpan file
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + path.extname(file.originalname)); // Nama file yang disimpan
+  },
 });
 
+const upload = multer({ storage: storage });
+
+router.use(express.json());
+router.use(express.urlencoded({ extended: true }));
+
+router.post('/upload_and_add_forum', protect, upload.single('image'), async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+    const forum = req.body.isi;
+    const uploadedFile = req.file;
+
+   if (!uploadedFile) {
+    const forumbaru = await ForumModel.create({
+        id_user: loggedInUser.id,
+        name: loggedInUser.name,
+        fill: forum,
+        image: null,
+      });
+  
+      res.status(200).json({
+        error: false,
+        message: "Tambah forum berhasil",
+        Ulasan: {
+          id_user: forumbaru.id_user,
+          name: forumbaru.name,
+          isi: forumbaru.fill,
+          image: forumbaru.image,
+        },
+      });
     
+   }
+
+   const filePath = path.join('images', uploadedFile.filename);
+   const fileUrl = req.protocol + '://' + req.get('host') + '/' + filePath;
+
+    const forumbaru = await ForumModel.create({
+      id_user: loggedInUser.id,
+      name: loggedInUser.name,
+      fill: forum,
+      image: fileUrl,
+    });
+
+    res.status(200).json({
+      error: false,
+      message: "Tambah forum berhasil",
+      Ulasan: {
+        id_user: forumbaru.id_user,
+        name: forumbaru.name,
+        isi: forumbaru.fill,
+        image: forumbaru.image,
+      },
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      error: true,
+      message: 'Terjadi kesalahan saat mengunggah file atau menambah forum.',
+    });
+  }
+});
+
+router.get('/test', async (req, res) => {
+  res.status(201).json({ message: 'lms access!' });
+});
+
+router.post('/add_forum', protect, async (req, res) => {
+  try {
+    const forum = req.body;
+    const loggedInUser = req.user;
+
+    const forumbaru = await ForumModel.create({
+      id_user: loggedInUser.id,
+      name: loggedInUser.name,
+      fill: forum.isi,
+      image: forum.image,
+    });
+
+    return res.status(200).json({
+      errr: false,
+      message: "Tambah forum berhasil",
+      Ulasan: {
+        id_user: forumbaru.id_user,
+        name: forumbaru.name,
+        isi: forumbaru.fill,
+        image: forumbaru.image,
+      },
+    });
+
+  } catch (error) {
+    console.error(error);
+    res.status(400).json({
+      error: true,
+      message: 'Kesalahan saat mengunggah forum'
+    });
+  }
+});
+
 router.use(erorHandlerMiddleware);
 
 module.exports = router;
